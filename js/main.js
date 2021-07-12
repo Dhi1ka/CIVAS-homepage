e1e9d25f31244150eee01e21f0a19f5bc25c477b
Dhi1ka <57865742+Dhi1ka@users.noreply.github.com>
2021-07-12 20:16:06 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,3 +17,14 @@ function backSidebar() {
 // function openOptions() {
 //   document.querySelectorAll("option").style.transition = "all 0.5s";
 // }
+
+function browseFile() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
+    let files = Array.from(input.files);
+    // console.log(files);
+    document.getElementById("inputFile").innerHTML = files;
+  };
+  input.click();
+}
