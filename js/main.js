e3d1b2e7e9260c76dce70381e3cb625d9c6a2c48
Dhi1ka <57865742+Dhi1ka@users.noreply.github.com>
2021-07-31 10:08:38 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,6 +39,24 @@ $('.modal-toggle').on('click', function(e) {
   $('.modal').toggleClass('is-visible');
 });
 
+// modal toggle list
+$('.modal-toggle-list').on('click', function(e) {
+  e.preventDefault();
+  $('.modal-list').toggleClass('is-visible');
+});
+
+// modal toggle accept
+$('.modal-toggle-accept').on('click', function(e) {
+  e.preventDefault();
+  $('.modal-accept').toggleClass('is-visible');
+});
+
+// modal toggle decline
+$('.modal-toggle-decline').on('click', function(e) {
+  e.preventDefault();
+  $('.modal-decline').toggleClass('is-visible');
+});
+
 // browse file
 function browseFile() {
   let input = document.createElement('input');
